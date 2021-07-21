21c3252888e5b3ecfe233006eb5a8eebed750690
josejamilena1 <jose.jamilena.ciber@gmail.com>
2021-07-21 10:18:18 +0200

    init

---
+++ src/main/java/org/example/api/beans/DataSetList.java
@@ -0,0 +1,42 @@
+
+package org.example.api.beans;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "total",
+    "apis"
+})
+public class DataSetList {
+
+    @JsonProperty("total")
+    private Integer total;
+    @JsonProperty("apis")
+    private List<Api> apis = new ArrayList<Api>();
+
+    @JsonProperty("total")
+    public Integer getTotal() {
+        return total;
+    }
+
+    @JsonProperty("total")
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    @JsonProperty("apis")
+    public List<Api> getApis() {
+        return apis;
+    }
+
+    @JsonProperty("apis")
+    public void setApis(List<Api> apis) {
+        this.apis = apis;
+    }
+
+}
